2f51c17a30cf07c0c7f1c1d10049c1ca78e9359f
anishbhattacharjee <33443758+anishbhattacharjee@users.noreply.github.com>
2018-05-02 17:07:22 +0530

    Add files via upload

--- my_server.php
+++ my_server.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once("SocketServer2.class.php"); // Include the File
-$server = new SocketServer("172.30.111.186" ,  8000); // Create a Server binding to the given ip address and listen to port 31337 for connections
+$server = new SocketServer("172.30.111.186" ,  8443); // Create a Server binding to the given ip address and listen to port 31337 for connections
 $server->max_clients = 10; // Allow no more than 10 people to connect at a time
 $server->hook("CONNECT","handle_connect"); // Run handle_connect every time someone connects
 $server->hook("INPUT","handle_input"); // Run handle_input whenever text is sent to the server
